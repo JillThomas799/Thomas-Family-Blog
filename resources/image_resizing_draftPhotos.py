19cb06cf1339cb3569327891ff2b283a4e639389
JillThomas799 <jillthomas532016@outlook.com>
2023-06-08 20:28:16 +0100

    June 7th 2022 blog entry

---
+++ resources/image_resizing_draftPhotos.py
@@ -0,0 +1,18 @@
+import os
+from PIL import Image
+
+
+rootdir = r'C:\Users\JillT\OneDrive\Documents\Computing\Web Development\Personal Web projects\Thomas-Family-Blog\resources\draftPhotos'
+destdir = r'C:\Users\JillT\OneDrive\Documents\Computing\Web Development\Personal Web projects\Thomas-Family-Blog\resources\images'
+files = os.listdir(rootdir)
+print(files)
+
+for image_name in files:
+    image_path = os.path.join(rootdir, image_name)
+    if os.path.isdir(image_path):
+        continue
+    image = Image.open(image_path)
+    image.thumbnail((500,500),Image.ANTIALIAS)
+    image.save(os.path.join(destdir, image_name ),"JPEG")
+
+
