234d6df87bcb9ab95ae0861cb76d7f094bcf2d25
JillThomas799 <jillthomas532016@outlook.com>
2023-11-13 16:50:51 +0000

    Update October blog

---
+++ script.js
@@ -0,0 +1,22 @@
+const JSX = 
+<div id="myDiv">
+            <ul>
+                <li><a class = "button" href = "index.html#button" title = "Click here to return to menu button "><span class="text">Menu:</span></a></li>
+                <li><a class = "About-us" href = "index.html#about_us" title = "Click here to find out more about us "><span class="text">About us</span></a></li>
+                <li><a class="Members" href = "index.html#Members" title = "Click here to go to find out more about the individual family members"><span class="text">Members</span></a></li>
+                <li><div class="text_left_align" >Choose month/event:</div></li>
+            </ul>
+                    <div id="menu">
+                        <ul>
+                                <li><a class="nav-link" href = "April_2023.html"  title = "Click here to go to April 2023 blogs">April 2023</a></li>
+                                <li><a class="nav-link" href = "May_2023.html"  title = "Click here to go to May 2023 blogs">May 2023</a></li>
+                                <li><a class="nav-link" href = "June_2023.html" id="menu" title = "Click here to go to June 2023 blogs">June 2023</a></li>
+                                <li><a class="nav-link" href = "Auvergne_holiday.html" id="menu" title = "Click here to go to Auvergne holiday, June 2023">Auvergne holiday</a></li>
+                                <li><a class="nav-link" href = "July_2023.html" id="menu" title = "Click here to go to July 2023 blogs">July 2023</a></li>
+                                <li><a class="nav-link" href = "August_2023.html" id="menu" title = "Click here to go to August 2023 blogs">August 2023</a></li>
+                                <li><a class="nav-link" href = "September_2023.html" id="menu" title = "Click here to go to September 2023 blogs">September 2023</a></li>
+                                <li><a class="nav-link" href = "October_2023.html" id="menu" title = "Click here to go to October 2023 blogs">October 2023</a></li>
+                                <li><a class="nav-link" href=  "Wales_holiday_2023.html" id="menu" title = "Click here to go to Wales holiday, October 2023">Wales holiday October 2023</a></li>
+                        </ul>
+                    </div>
+</div>
